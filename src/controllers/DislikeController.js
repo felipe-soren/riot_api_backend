99d85499960f15e87d8d3e1adfb6fb5dc3c6c9d5
felipe-soren <30413876+felipe-soren@users.noreply.github.com>
2019-08-28 22:03:51 -0300

    Update DislikeController.js

--- src/controllers/DislikeController.js
+++ src/controllers/DislikeController.js
@@ -1,16 +1,17 @@
 const Summoner = require('../models/Summoner')
 
 module.exports = {
-  async store(req, res){
-    const { name } = req.params;
+  async store(req, res) {
+    const { name } = req.headers;
+    console.log(name)
     const summoner = await Summoner.findOne({summonerName : name});
+    console.log(summoner)
     if(!summoner){
       return res.json({error : "user not exists"})
     }
+
     summoner.dislike ++;
-    console.log(summoner.dislike)
     await summoner.save();
     return res.json(summoner);
-  },
-
+  }
 }
